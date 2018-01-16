de2af4b68c621d7bc6bdce34ab0a60277ee7b6b4
edsonsalvador30 <edsonsalvador30>
2018-01-16 13:49:28 +0000

    vai

---
+++ src/main/webapp/js/blockly/Bloco.blockly.js
@@ -0,0 +1,301 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Bloco = window.blockly.js.blockly.Bloco || {};
+
+var item;
+
+/**
+ * Bloco
+ */
+window.blockly.js.blockly.Bloco.Executar = function() {
+	item = this.cronapi.screen.getValueOfField("vars.search");
+	if (item == 'um' || item == 'UM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'tocantins' || item == 'TO') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do norte' || item == 'RN') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do sul' || item == 'RS') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Minas' || item == 'MG') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'amazonas' || item == 'AM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'brasilia' || item == 'DF') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sergipe' || item == 'SE') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'espirito santo' || item == 'ES') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio de janeiro' || item == 'RJ') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sao Paulo' || item == 'SP') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else {
+		this.cronapi.screen.notify('info', 'Estado não cadastrado');
+	}
+	if (item == 'um' || item == 'UM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'tocantins' || item == 'TO') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do norte' || item == 'RN') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do sul' || item == 'RS') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Minas' || item == 'MG') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'amazonas' || item == 'AM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'brasilia' || item == 'DF') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sergipe' || item == 'SE') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'espirito santo' || item == 'ES') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio de janeiro' || item == 'RJ') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sao Paulo' || item == 'SP') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else {
+		this.cronapi.screen.notify('info', 'Estado não cadastrado');
+	}
+	if (item == 'um' || item == 'UM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'tocantins' || item == 'TO') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do norte' || item == 'RN') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do sul' || item == 'RS') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Minas' || item == 'MG') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'amazonas' || item == 'AM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'brasilia' || item == 'DF') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sergipe' || item == 'SE') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'espirito santo' || item == 'ES') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio de janeiro' || item == 'RJ') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sao Paulo' || item == 'SP') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else {
+		this.cronapi.screen.notify('info', 'Estado não cadastrado');
+	}
+	if (item == 'um' || item == 'UM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'tocantins' || item == 'TO') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do norte' || item == 'RN') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do sul' || item == 'RS') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Minas' || item == 'MG') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'amazonas' || item == 'AM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'brasilia' || item == 'DF') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sergipe' || item == 'SE') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'espirito santo' || item == 'ES') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio de janeiro' || item == 'RJ') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sao Paulo' || item == 'SP') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else {
+		this.cronapi.screen.notify('info', 'Estado não cadastrado');
+	}
+	if (item == 'um' || item == 'UM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'tocantins' || item == 'TO') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do norte' || item == 'RN') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do sul' || item == 'RS') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Minas' || item == 'MG') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'amazonas' || item == 'AM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'brasilia' || item == 'DF') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sergipe' || item == 'SE') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'espirito santo' || item == 'ES') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio de janeiro' || item == 'RJ') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sao Paulo' || item == 'SP') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else {
+		this.cronapi.screen.notify('info', 'Estado não cadastrado');
+	}
+	if (item == 'um' || item == 'UM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'tocantins' || item == 'TO') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do norte' || item == 'RN') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do sul' || item == 'RS') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Minas' || item == 'MG') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'amazonas' || item == 'AM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'brasilia' || item == 'DF') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sergipe' || item == 'SE') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'espirito santo' || item == 'ES') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio de janeiro' || item == 'RJ') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sao Paulo' || item == 'SP') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else {
+		this.cronapi.screen.notify('info', 'Estado não cadastrado');
+	}
+	if (item == 'um' || item == 'UM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'tocantins' || item == 'TO') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do norte' || item == 'RN') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do sul' || item == 'RS') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Minas' || item == 'MG') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'amazonas' || item == 'AM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'brasilia' || item == 'DF') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sergipe' || item == 'SE') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'espirito santo' || item == 'ES') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio de janeiro' || item == 'RJ') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sao Paulo' || item == 'SP') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else {
+		this.cronapi.screen.notify('info', 'Estado não cadastrado');
+	}
+	if (item == 'um' || item == 'UM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'tocantins' || item == 'TO') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do norte' || item == 'RN') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio grande do sul' || item == 'RS') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Minas' || item == 'MG') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'amazonas' || item == 'AM') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'brasilia' || item == 'DF') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sergipe' || item == 'SE') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'espirito santo' || item == 'ES') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'rio de janeiro' || item == 'RJ') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else if (item == 'Sao Paulo' || item == 'SP') {
+		this.cronapi.screen
+				.notify('info', String('Cadastrado ') + String(item));
+	} else {
+		this.cronapi.screen.notify('info', 'Estado não cadastrado');
+	}
+}
